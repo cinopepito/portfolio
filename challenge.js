0437403cb10fb00b30e597dc8b825f1da09af708
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-13 19:10:42 +0100

    line animals have been added

--- challenge.js
+++ challenge.js
@@ -1,41 +1,46 @@
 /* this is an algorithm that track the sore my friends */
 var scores = {
-    "abla":{
-        score:10,
-        age:12,
-        quartier:'baguida'
-    },
-    "akou":{
-        score:10,
-        age:12,
-        quartier:'baguida'
-    },
-    "koko":{
-        score:10,
-        age:12,
-        quartier:'baguida'
-    },
-    "yaovi":{
-        score:10,
-        age:12,
-        quartier:'baguida'
-    },
-}
-var ablaScore = scores['abla']['score']+=5;
-console.log('le score de abla est' ,ablaScore);
+  abla: {
+    score: 10,
+    age: 12,
+    quartier: "baguida",
+  },
+  akou: {
+    score: 10,
+    age: 12,
+    quartier: "baguida",
+  },
+  koko: {
+    score: 10,
+    age: 12,
+    quartier: "baguida",
+  },
+  yaovi: {
+    score: 10,
+    age: 12,
+    quartier: "baguida",
+  },
+};
+var ablaScore = (scores["abla"]["score"] += 5);
+console.log("le score de abla est", ablaScore);
 
-var akouScore = scores['akou']['score']+=10;
+var akouScore = (scores["akou"]["score"] += 10);
 console.log(akouScore);
 
-var kokoScore = scores['koko']['score']+=3;
+var kokoScore = (scores["koko"]["score"] += 3);
 console.log(kokoScore);
 
-var yaoviScore = scores['yaovi']['score']+=20;
+var yaoviScore = (scores["yaovi"]["score"] += 20);
 console.log(yaoviScore);
 
 var myCrazyObject = {
-    "name": "A ridiculous object",
-    "some array": [7, 9, { purpose: "confusion", number: 123 }, 3.3],
-    "random animal": "Banana Shark"
-    };
-    console.log(myCrazyObject['some array'][2].number)
+  name: "A ridiculous object",
+  "some array": [7, 9, { purpose: "confusion", number: 123 }, 3.3],
+  "random animal": "Banana Shark",
+};
+console.log(myCrazyObject["some array"][2].number);
+
+var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
+for (var i = 0; i < animals.length; i++) {
+  console.log('awesome',animals[i]);
+}
